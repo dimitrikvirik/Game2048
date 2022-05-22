1fd59a9309a92137bdc6ec77e49da3fd75da7e0d
FaultHappens <tskitishvili01@gmail.com>
2022-05-22 15:10:21 +0400

    Added Swipe gesture listeners in game fragment, also bg grids for power 2-11, changed primary colors

--- app/src/main/java/git/dimitrikvirik/game2048/presentation/ui/fragment/GameFragment.kt
+++ app/src/main/java/git/dimitrikvirik/game2048/presentation/ui/fragment/GameFragment.kt
@@ -1,16 +1,12 @@
 package git.dimitrikvirik.game2048.presentation.ui.fragment
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import git.dimitrikvirik.game2048.R
+import git.dimitrikvirik.game2048.data.enum.Swipe
 import git.dimitrikvirik.game2048.databinding.FragmentGameBinding
-import git.dimitrikvirik.game2048.databinding.FragmentScoreBoardBinding
-import git.dimitrikvirik.game2048.presentation.vm.ChooseNickNameFragmentVM
 import git.dimitrikvirik.game2048.presentation.vm.GameFragmentVM
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -33,13 +29,119 @@ class GameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         nickname = context?.getSharedPreferences("mySharedPreferences", Context.MODE_PRIVATE)?.getString("name", "None")?: "None"
         bestScore = context?.getSharedPreferences("mySharedPreferences", Context.MODE_PRIVATE)?.getInt("bestScore", 0)?: 0
+        context?.let {
+            binding.boardGL.setOnTouchListener(object: OnSwipeTouchListener(it) {
+                override fun onSwipeLeft() {
+                    handleSwipe(Swipe.LEFT)
+                }
+
+                override fun onSwipeRight() {
+                    handleSwipe(Swipe.RIGHT)
+                }
+
+                override fun onSwipeTop() {
+                    handleSwipe(Swipe.TOP)
+                }
+
+                override fun onSwipeBottom() {
+                    handleSwipe(Swipe.BOTTOM)
+                }
+            })
+        }
     }
 
-    fun updateBestScore(score: Int){
+    private fun updateBestScore(score: Int){
         if(score > bestScore){
             context?.getSharedPreferences("mySharedPreferences", Context.MODE_PRIVATE)?.edit()?.putInt("bestScore", score)?.apply()
         }
     }
 
+    private fun handleSwipe(swipe: Swipe){
+        when(swipe){
+            Swipe.LEFT-> {
 
+            }
+            Swipe.RIGHT-> {
+
+            }
+            Swipe.BOTTOM-> {
+
+            }
+            Swipe.TOP-> {
+
+            }
+        }
+    }
+
+    private fun moveCell(moveFrom: Pair<Int, Int>, moveTo: Pair<Int, Int>){
+
+    }
+
+
+}
+
+open class OnSwipeTouchListener(ctx: Context) : View.OnTouchListener {
+
+    private val gestureDetector: GestureDetector
+
+    companion object {
+
+        private val SWIPE_THRESHOLD = 100
+        private val SWIPE_VELOCITY_THRESHOLD = 100
+    }
+
+    init {
+        gestureDetector = GestureDetector(ctx, GestureListener())
+    }
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        return gestureDetector.onTouchEvent(event)
+    }
+
+    private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+
+
+        override fun onDown(e: MotionEvent): Boolean {
+            return true
+        }
+
+        override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+            var result = false
+            try {
+                val diffY = e2.y - e1.y
+                val diffX = e2.x - e1.x
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight()
+                        } else {
+                            onSwipeLeft()
+                        }
+                        result = true
+                    }
+                } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeBottom()
+                    } else {
+                        onSwipeTop()
+                    }
+                    result = true
+                }
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+            }
+
+            return result
+        }
+
+
+    }
+
+    open fun onSwipeRight() {}
+
+    open fun onSwipeLeft() {}
+
+    open fun onSwipeTop() {}
+
+    open fun onSwipeBottom() {}
 }
